3324aab34067cb3d6230fbf5242d576cfb447781
Dennelle <dennelle.walton@gmail.com>
2023-10-03 15:01:39 -0400

    completed the practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 /*----- constants -----*/
 const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
 const RPS_LOOKUP = {
-    r: 'imgs/rock.png',
-    p: 'imgs/paper.png',
-    s: 'imgs/scissors.png'
+    r: {img: 'imgs/rock.png', beats: 's'},
+    p: {img: 'imgs/paper.png', beats: 'r'},
+    s: {img: 'imgs/scissors.png', beats: 'p'}
 };
 
 /*----- app's state (variables) -----*/
@@ -20,9 +20,11 @@ let winner;
 /*----- cached element references -----*/
 const pResultEL = document.getElementById('p-result');
 const cResultEL = document.getElementById('c-result');
+const countdownEL = document.getElementById('c-result');
+
 
 /*----- event listeners -----*/
-
+document.querySelector('main').addEventListener('click', handleChoice);
 
 /*----- functions -----*/
 init(); //calling the init function
@@ -42,6 +44,36 @@ function init(){
     render();
 }
 
+// In response to user interaction (player makes a move),
+// we update all impacted state, then finally, call render()
+function handleChoice(evt) {
+    //guards to protect if the user clicks something other than the 3 buttons.
+    if (evt.target.tagName !== 'BUTTON') return;
+    // PLayer has made a choice
+    results.p = evt.target.innerText.toLowerCase();
+    //compute a random choice for the computer
+    results.c = getRandomRPS();
+    winner = getWinner();
+    scores[winner] += 1;
+
+    render();
+
+}
+
+function getWinner() {
+    if (results.p === results.c) return 't';
+    return RPS_LOOKUP[results.p].beats === results.c ? 'p' : 'c';
+
+
+}
+function getRandomRPS() {
+    const rps = Object.keys(RPS_LOOKUP);
+    const rndIdx = Math.floor(Math.random() * rps.length);
+
+
+    return rps[rndIdx];
+}
+
 function renderScores() {
 for (let key in scores) {
     const scoreEl = document.getElementById(`${key}-score`);
@@ -50,11 +82,33 @@ for (let key in scores) {
 }
 
 function renderResults() {
-    pResultEL.src = RPS_LOOKUP[results.p];
-    cResultEL.src = RPS_LOOKUP[results.c];
+    pResultEL.src = RPS_LOOKUP[results.p].img;
+    cResultEL.src = RPS_LOOKUP[results.c].img;
+    pResultEL.style.borderColor = winner === 'p' ? 'grey' : 'white';
+    cResultEL.style.borderColor = winner === 'c' ? 'grey' : 'white';
 }
-
+//render functions or visual all state to the DOM
 function render() {
+    renderCountdown(function() {
     renderScores();
     renderResults();
+    });
+
+    function renderCountdown(cb) {
+    let count = 3;
+    AUDIO.currentTime=0;
+    countdownEL.style.visibility = 'visible';
+    countdownEL.innerText = count;
+    const timerId = setInterval(function() {
+        count --;
+        if (count) {
+            countdownEL.innerText = count;
+        } else {
+            clearInterval(timerId);
+            countdownEL.style.visibility = 'hidden';
+            cb();
+        }
+    }, 1000 );
+    }
+
 }
